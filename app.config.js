80eec83db85b0626113e2baa46ae4d5a3e57a31f
Irfan <irfanhadi619@gmail.com>
2026-05-30 13:15:28 +0700

    chore(build): update npm package and update expo config parameters

--- app.config.js
+++ app.config.js
@@ -5,76 +5,78 @@ export default () => {
   console.log("ENV:", ENV);
 
   return {
-    name: isDev ? "Sentosa ATK (Dev)" : "Sentosa ATK",
-    slug: "sistem-inventaris-sentosa-atk",
-    version: "1.4.1",
-    orientation: "portrait",
-    icon: "./assets/images/icon.png",
-    scheme: "sisteminventarissentosaatk",
-    userInterfaceStyle: "automatic",
+    expo: {
+      name: isDev ? "Sentosa ATK (Dev)" : "Sentosa ATK",
+      slug: "sistem-inventaris-sentosa-atk",
+      version: "1.4.1",
+      orientation: "portrait",
+      icon: "./assets/images/icon.png",
+      scheme: "sisteminventarissentosaatk",
+      userInterfaceStyle: "automatic",
 
-    ios: {
-      buildNumber: "14",
-      supportsTablet: true
-    },
-
-    android: {
-      package: isDev
-        ? "com.anonymous.sisteminventarissentosaatk.dev"
-        : "com.anonymous.sisteminventarissentosaatk",
-      versionCode: 14,
-      permissions: ["REQUEST_INSTALL_PACKAGES"],
-      predictiveBackGestureEnabled: false,
-      adaptiveIcon: {
-        backgroundColor: "#E6F4FE",
-        foregroundImage: "./assets/images/android-icon-foreground.png",
-        backgroundImage: "./assets/images/android-icon-background.png",
-        monochromeImage: "./assets/images/android-icon-monochrome.png"
+      ios: {
+        buildNumber: "14",
+        supportsTablet: true
       },
-      googleServicesFile: "./google-services.json"
-    },
 
-    updates: {
-      url: "https://u.expo.dev/d9e98c14-1318-432b-88a3-3889af0bf951",
-      fallbackToCacheTimeout: 0
-    },
+      android: {
+        package: isDev
+          ? "com.anonymous.sisteminventarissentosaatk.dev"
+          : "com.anonymous.sisteminventarissentosaatk",
+        versionCode: 14,
+        permissions: ["REQUEST_INSTALL_PACKAGES"],
+        predictiveBackGestureEnabled: false,
+        adaptiveIcon: {
+          backgroundColor: "#E6F4FE",
+          foregroundImage: "./assets/images/android-icon-foreground.png",
+          backgroundImage: "./assets/images/android-icon-background.png",
+          monochromeImage: "./assets/images/android-icon-monochrome.png"
+        },
+        googleServicesFile: "./google-services.json"
+      },
 
-    runtimeVersion: {
-      policy: "appVersion"
-    },
+      updates: {
+        url: "https://u.expo.dev/d9e98c14-1318-432b-88a3-3889af0bf951",
+        fallbackToCacheTimeout: 0
+      },
 
-    web: {
-      output: "static",
-      favicon: "./assets/images/favicon.png"
-    },
+      runtimeVersion: {
+        policy: "appVersion"
+      },
 
-    plugins: [
-      "expo-router",
-      [
-        "expo-splash-screen",
-        {
-          image: "./assets/images/splash-icon.png",
-          imageWidth: 200,
-          resizeMode: "contain",
-          backgroundColor: "#ffffff",
-          dark: {
-            backgroundColor: "#ffffff"
+      web: {
+        output: "static",
+        favicon: "./assets/images/favicon.png"
+      },
+
+      plugins: [
+        "expo-router",
+        [
+          "expo-splash-screen",
+          {
+            image: "./assets/images/splash-icon.png",
+            imageWidth: 200,
+            resizeMode: "contain",
+            backgroundColor: "#ffffff",
+            dark: {
+              backgroundColor: "#ffffff"
+            }
           }
-        }
+        ],
+        "expo-sqlite",
+        "@react-native-firebase/app"
       ],
-      "expo-sqlite",
-      "@react-native-firebase/app"
-    ],
 
-    experiments: {
-      typedRoutes: true
-    },
+      experiments: {
+        typedRoutes: true
+      },
 
-    extra: {
-      router: {},
-      env: ENV,
-      eas: {
-        projectId: "d9e98c14-1318-432b-88a3-3889af0bf951"
+      extra: {
+        router: {},
+        env: ENV,
+        eas: {
+          projectId: "d9e98c14-1318-432b-88a3-3889af0bf951"
+        }
       }
     }
   };
